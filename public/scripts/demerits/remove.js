9bb55ae0744d3025c736cb30ff0652c06a4879fc
lee-rimmer <lee.rimmer@bglgroup.co.uk>
2022-05-30 13:46:22 +0100

    Remove unnecessary code

--- public/scripts/demerits/remove.js
+++ public/scripts/demerits/remove.js
@@ -1,7 +1,5 @@
-(function() {
-    for (const removeButton of document.querySelectorAll('[data-type="button"][data-operation="remove"]')) removeButton.addEventListener('click', remove);
-})();
-
 function remove() {
     this.closest('section').remove();
 };
+
+for (const removeButton of document.querySelectorAll('[data-type="button"][data-operation="remove"]')) removeButton.addEventListener('click', remove);
