85ad48558de61a28150eb25141a5e15ae979fbfb
lee-rimmer <lee.rimmer@bglgroup.co.uk>
2022-06-24 09:06:41 +0100

    Reorder years and change function name

--- utilities/seasons.js
+++ utilities/seasons.js
@@ -1,4 +1,4 @@
-function seasonDates(year) {
+function dates(year) {
     return {
         endDate: new Date(year + 1, 2, 31),
         startDate: new Date (year, year === 0 ? 0 : 3, 1)
@@ -8,8 +8,8 @@ function seasonDates(year) {
 function years() {
     const currentYear = new Date().getFullYear();
     const startYear = 2021;
-    return Array.from({ length: currentYear - startYear + 1 }, (v, i) => startYear + i)
+    return Array.from({ length: currentYear - startYear + 1 }, (v, i) => currentYear - i)
         .map(year => ({ year, current: year === currentYear }));
 };
 
-module.exports = { seasonDates, years };
+module.exports = { dates, years };
