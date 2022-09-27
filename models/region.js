f7a529130695b74013eabf22b15c9e8b73cfff06
lee-rimmer <lee.rimmer@comparethemarket.com>
2022-09-27 15:35:18 +0100

    Reference country model

--- models/region.js
+++ models/region.js
@@ -1,18 +1,16 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const { COUNTRY_CODES } = require('../constants');
-
 const RegionSchema = new Schema({
-    name: String,
     code: {
         type: String,
         unique: true
     },
     country: {
-        type: String,
-        enum: COUNTRY_CODES.map(({ name }) => name)
-    }
+        ref: 'Country',
+        type: Schema.Types.ObjectId
+    },
+    name: String
 });
 
 module.exports = mongoose.model('Region', RegionSchema);
