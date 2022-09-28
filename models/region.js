8459269ced09a163c855860a10f337cbab5cfa65
lee-rimmer <lee.rimmer@comparethemarket.com>
2022-09-28 17:06:02 +0100

    Update statics so that it works

--- models/region.js
+++ models/region.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const Country = require('./country');
+
 const RegionSchema = new Schema({
     code: {
         type: String,
@@ -13,12 +15,15 @@ const RegionSchema = new Schema({
     name: String
 });
 
-RegionSchema.statics.findByCountry = async function(country) {
-    return await this.find().populate({ path: 'country', match: { country } }).populate('country');
+RegionSchema.statics.findByCountry = async function(value) {
+    const options = /\w{3}/.test(value) ? { code: value } : { country: value };
+    const country = await Country.find(options);
+    return await this.find({ country }).populate('country');
 };
 
-RegionSchema.statics.findByCountryCode = async function(code) {
-    return await this.find().populate({ path: 'country', match: { code } }).populate('country');
-};
+// why is this no longer working?
+// RegionSchema.statics.findByCountry = async function(code) {
+//     return await this.find().populate({ path: 'country', match: { code } }).populate('country');
+// };
 
 module.exports = mongoose.model('Region', RegionSchema);
