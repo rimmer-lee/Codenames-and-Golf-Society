ebef25d22c55feb47407bb403df403f6d5e83d76
lee-rimmer <lee.rimmer@comparethemarket.com>
2023-01-03 12:50:39 +0000

    Tidy and update logic to return relevant seasons

--- utilities/seasons.js
+++ utilities/seasons.js
@@ -1,15 +1,18 @@
+// hard code 2021 as start year for now
+const startOfTime = 2021;
+
 function dates(year) {
     return {
-        endDate: new Date(year + 1, 2, 31),
-        startDate: new Date (year, year === 0 ? 0 : 3, 1)
+        endDate: new Date(Date.UTC(year + 1, 2, 31)),
+        startDate: new Date(Date.UTC(year, year === startOfTime ? 0 : 3, 1))
     };
 };
 
 function years() {
     const currentYear = new Date().getFullYear();
-    const startYear = 2021;
-    return Array.from({ length: currentYear - startYear + 1 }, (v, i) => currentYear - i)
-        .map(year => ({ year, current: year === currentYear }));
+    const currentSeason = currentYear - +(new Date() < new Date(Date.UTC(currentYear, 2, 31)));
+    return Array.from({ length: currentSeason - startOfTime + 1 }, (_, i) => currentSeason - i)
+        .map(year => ({ year, current: year === currentSeason }));
 };
 
 module.exports = { dates, years };
